135fad690451a07011b279d052c28c475b47c31b
Dylanson25 <dvillarreal0@ucol.mx>
2023-07-21 15:28:44 -0600

    colors config

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import router from './router';
 import Oruga from '@oruga-ui/oruga-next';
 import { bulmaConfig } from '@oruga-ui/theme-bulma';
 import '@oruga-ui/theme-bulma/dist/bulma.css';
+import '@/assets/sass/style.sass';
 
 const app = createApp(App);
 
