9f4a918e6a67d664b0be08f1d8ff647742617573
Dylanson25 <dvillarreal0@ucol.mx>
2023-07-21 13:18:31 -0600

    implement Oruga with Bulma

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,13 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-createApp(App).use(router).mount('#app');
+//Material ui
+import Oruga from '@oruga-ui/oruga-next';
+import { bulmaConfig } from '@oruga-ui/theme-bulma';
+import '@oruga-ui/theme-bulma/dist/bulma.css';
+
+const app = createApp(App);
+
+app.use(Oruga, bulmaConfig);
+app.use(router);
+app.mount('#app');
