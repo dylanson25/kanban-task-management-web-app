77b9d8d818a10285989c41735058aef245c09f74
Dylanson25 <dvillarreal0@ucol.mx>
2023-08-24 20:55:39 -0600

    Change bulma to bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-
 //Material ui
 import Oruga from '@oruga-ui/oruga-next';
-import { bulmaConfig } from '@oruga-ui/theme-bulma';
-import '@oruga-ui/theme-bulma/dist/bulma.css';
+import { bootstrapConfig } from '@oruga-ui/theme-bootstrap';
+import '@oruga-ui/theme-bootstrap/dist/bootstrap.css';
 import '@/assets/sass/style.sass';
 
 const app = createApp(App);
 
-app.use(Oruga, bulmaConfig);
+app.use(Oruga, bootstrapConfig);
 app.use(router);
 app.mount('#app');
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,6 +2,7 @@
   <section>
     <div class="odocs-spaced">
       <o-button variant="primary">Primary</o-button>
+      <o-button variant="secondary">secondary</o-button>
       <o-button variant="success">Success</o-button>
       <o-button variant="danger">Danger</o-button>
       <o-button variant="warning">Warning</o-button>
